37c448d8ed608c76bc87b23b9e2dc0e852da88cc
Andres Mallea <andresmallea3@gmail.com>
2026-05-29 23:43:48 -0400

    prueba unitaria 3

---
+++ src/app/core/services/notification.service.spec.ts
@@ -0,0 +1,17 @@
+import { NotificationService } from './notification.service';
+import { NotificationType } from '../models/enums.model';
+
+describe('NotificationService', () => {
+  let service: NotificationService;
+
+  beforeEach(() => { service = new NotificationService(); });
+
+  it('debería emitir un mensaje de éxito', (done) => {
+    service.message$.subscribe(data => {
+      expect(data.msg).toBe('Guardado');
+      expect(data.type).toBe(NotificationType.SUCCESS);
+      done();
+    });
+    service.notify('Guardado', NotificationType.SUCCESS);
+  });
+});
